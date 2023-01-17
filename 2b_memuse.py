feff00608aac0d4a53cc465a9a1582f5331eaac1
KENTO <42343541+kento2247@users.noreply.github.com>
2023-01-17 22:33:58 +0900

    カラー選択可能

--- 2b_memuse.py
+++ 2b_memuse.py
@@ -6,29 +6,47 @@ import tkinter.ttk as ttk
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 import random
-
+from tkinter import colorchooser
 # データ準備
 # in_data = [124, 60, 70, 133, 12, 19, 66, 52, 62, 2, 99, 82, 33]
 in_data = []
 in_data_len = 0
 data = [[]]
+data_len = 0
 step = 0
 max_step = 2000
 is_sort = 0
 auto_state = 0
 auto_speed = 5
+is_sorted = 0
+start_color = [0, 1, 1]
+end_color = [1, 0, 0]
+only_swap = 0
+guide_bar_color = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
 
 
 def redraw():
     plt.cla()
-    x = np.arange(len(in_data))
+    x = np.arange(in_data_len)
     y = data[step][3:]
-    bar_list = ax.bar(x, y, color="gray")
+    # bar_list = ax.bar(x, y, color="gray") #変更
+    color_array = []
+    for h in y:
+        color_vector = [0, 0, 0]
+        for i in range(3):
+            color_vector[i] = start_color[i] + \
+                (end_color[i]-start_color[i])/in_data_len*h
+        color_array.append(color_vector)
+    bar_list = ax.bar(x, y, color=color_array)
     if(data[step][0] != -1):  # [start,end,range]
         # data[step][0]はswap元を指す
-        bar_list[data[step][0]].set_color("red")
-        bar_list[data[step][1]].set_color("red")
-        bar_list[data[step][2]].set_color("blue")
+        bar_list[data[step][0]].set_color(guide_bar_color[0])
+        bar_list[data[step][1]].set_color(guide_bar_color[1])
+        bar_list[data[step][2]].set_color(guide_bar_color[2])
     canvas.draw()
 
 
@@ -38,20 +56,24 @@ def array_set(len):
     for i in range(1, len+1):
         # in_data.append(i)
         in_data.append(random.randint(0, len))
+    global data
+    data = [([-1, -1, -1]+in_data)]
+    global is_sorted
+    is_sorted = 0
 
 
 def do_sort():
     global data
     data = [([-1, -1, -1]+in_data)]
-    if(method.get() == "merge sort"):
-        # mergeSort(in_data)
-        pass
-    elif(method.get() == "quick sort"):
+    if(method.get() == "quick sort"):
         quickSort(0, len(in_data)-1)
-        # pass
     elif(method.get() == "simple sort"):
         simpleSort()
-        # pass
+    data.append([-1, -1, -1]+data[-1][3:])
+    global is_sorted
+    is_sorted = 1
+    global data_len
+    data_len = len(data)
 
 
 def method_selected(event):
@@ -76,6 +98,8 @@ def sort_selected(event):
 
 def btn_click_next():
     print('next')
+    if (is_sorted == 0):
+        do_sort()
     global auto_state
     if(auto_state == 1):
         global auto_speed
@@ -95,6 +119,8 @@ def btn_click_next():
 
 def btn_click_back():
     print('back')
+    if (is_sorted == 0):
+        do_sort()
     global auto_state
     if(auto_state == 1):
         global auto_speed
@@ -114,6 +140,9 @@ def btn_click_back():
 
 def btn_click_auto():
     print('auto')
+    global is_sorted
+    if (is_sorted == 0):
+        do_sort()
     global auto_state
     auto_state = (1-auto_state)
 
@@ -127,15 +156,31 @@ def btn_click_restart():
     redraw()
 
 
+def btn_click_start_color():
+    color = colorchooser.askcolor()
+    print(color)
+    global start_color
+    for (i, j) in zip(color[0], range(3)):
+        start_color[j] = i/255
+    btn_start.config(fg=color[1])
+    redraw()
+
+
+def btn_click_end_color():
+    color = colorchooser.askcolor()
+    print(color)
+    global end_color
+    for (i, j) in zip(color[0], range(3)):
+        end_color[j] = i/255
+    btn_end.config(fg=color[1])
+    redraw()
+
+
 def change_length(num):
     pointer = num.find(".")
     global in_data_len
-    in_data_len = int(num[0:pointer])
-    global lenstr
-    lenstr.set("data length="+str(in_data_len))
+    in_data_len = int(num)
     array_set(in_data_len)
-    global data
-    data = [([-1, -1, -1]+in_data)]
     btn_click_restart()
 
 
@@ -144,17 +189,16 @@ def auto_play():
     if(auto_state):
         global step
         global stepstr
-        arr_len = len(data)-1
-        if(step >= arr_len):
+        if(step >= data_len-1):
             auto_state = 0
         else:
-            step = step + 1 + arr_len//1000*auto_speed
-            if(step > arr_len):
-                step = arr_len
-        stepstr.set("speed="+str(auto_speed) +
-                    ", step="+str(step))
-        redraw()
-    root.after(10, auto_play)
+            step = step + 1 + data_len//100*auto_speed
+            if(step >= data_len-1):
+                step = data_len-1
+            stepstr.set("speed="+str(auto_speed) +
+                        ", step="+str(step))
+            redraw()
+    root.after(1, auto_play)
 
 
 def quickSort(left, right):
@@ -168,10 +212,12 @@ def quickSort(left, right):
     while(i < j):
         while(new_data[i] < pivot):
             i = i+1
-            data.append([left, right, i]+new_data)
+            if(only_swap == 0):
+                data.append([left, right, i]+new_data)
         while(new_data[j] > pivot):
             j = j-1
-            data.append([left, right, i]+new_data)
+            if(only_swap == 0):
+                data.append([left, right, j]+new_data)
         if(i <= j):
             buffer = new_data[i]
             new_data[i] = new_data[j]
@@ -190,7 +236,8 @@ def simpleSort():
         for j in range(i, len(in_data)):
             if (array[minpos] > array[j]):
                 minpos = j
-            data.append([i, j, minpos]+array)
+            if(only_swap == 0):
+                data.append([i, j, minpos]+array)
         buffer = array[i]
         array[i] = array[minpos]
         array[minpos] = buffer
@@ -202,11 +249,13 @@ if __name__ == "__main__":
     data[0] = ([-1]+in_data)
     root = tk.Tk()  # ウインドの作成
     root.title("sample")  # ウインドのタイトル
-    root.geometry("800x700")  # ウインドの大きさ
+    root.geometry("1080x720")  # ウインドの大きさ
     combo = str()
-    frame_left1 = tk.Frame()
-    frame_left2 = tk.Frame()
-    frame_right = tk.Frame()
+    frame_1 = tk.Frame()
+    frame_2 = tk.Frame()
+    frame_3 = tk.Frame()
+    frame_4 = tk.Frame()
+    frame_5 = tk.Frame()
 
     fig, ax = plt.subplots()
     x = np.arange(len(in_data))
@@ -224,59 +273,65 @@ if __name__ == "__main__":
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
     label = tk.Label(root, text="sort method")
-    label.pack(fill='none', padx=20, pady=4, in_=frame_left1)
+    label.pack(fill='none', padx=5, pady=4, in_=frame_1, side="top")
     label = tk.Label(root, text="default sort")
-    label.pack(fill='none', padx=20, pady=4, in_=frame_left1)
-    label = tk.Label(root, text="sample number")
-    label.pack(fill='none', padx=20, in_=frame_left1)
+    label.pack(fill='none', padx=5, pady=4, in_=frame_2, side="top")
+    label = tk.Label(root, text="sample length")
+    label.pack(fill='none', padx=5, in_=frame_3, side="top")
 
     global method
     option = ["quick sort", "simple sort"]  # 選択肢
     method = tk.StringVar()
     label = ttk.Combobox(root, values=option,
-                         textvariable=method, state="readonly")
+                         textvariable=method, state="readonly", justify="center", width=10)
     label.bind("<<ComboboxSelected>>", method_selected)
-    label.pack(fill='none', padx=20, pady=4, in_=frame_left2)
+    label.pack(fill='none', padx=5, in_=frame_1)
 
     global sort
     option = ["down", "up", "random"]  # 選択肢
     sort = tk.StringVar()
     label = ttk.Combobox(root, values=option,
-                         textvariable=sort, state="readonly")
+                         textvariable=sort, state="readonly", justify="center", width=10)
     label.bind("<<ComboboxSelected>>", sort_selected)
-    label.pack(fill='none', padx=20, in_=frame_left2)
+    label.pack(fill='none', padx=5, in_=frame_2)
 
-    scale = ttk.Scale(
+    scale = tk.Scale(
         root,
         orient=tk.HORIZONTAL,
         from_=0,
         to=512,
         command=change_length,
+        showvalue=True,
+        length=512/2
     )
-    scale.pack(padx=10, fill=tk.X, in_=frame_left2)
-
-    lenstr = tk.StringVar()
-    lenstr.set("data length="+str(in_data_len))
-    label = tk.Label(root, text="", textvariable=lenstr)
-    label.pack(fill='none', padx=20, side='bottom', in_=frame_right)
+    scale.pack(padx=20, fill=tk.X, in_=frame_3)
 
     stepstr = tk.StringVar()
     stepstr.set("step="+str(step))
     label = tk.Label(root, text="", textvariable=stepstr)
-    label.pack(fill='none', padx=20, pady=4, side='bottom', in_=frame_right)
+    label.pack(fill='none', padx=5, pady=4, side='bottom', in_=frame_5)
 
     # ボタン作成
-    btn = tk.Button(root, text='>', width=6, command=btn_click_next)
-    btn.pack(fill='none', padx=8, side='right', in_=frame_right)
-    btn = tk.Button(root, text='<', width=6, command=btn_click_back)
-    btn.pack(fill='none', padx=8, side='right', in_=frame_right)
-    btn = tk.Button(root, text='▷', width=6, command=btn_click_auto)
-    btn.pack(fill='none', padx=8, side='right', in_=frame_right)
-    btn = tk.Button(root, text='↩︎', width=6, command=btn_click_restart)
-    btn.pack(fill='none', padx=8, side='right', in_=frame_right)
+    btn_start = tk.Button(root, text='start color', width=6, fg="#00FFFF", background='#00ffff',
+                          command=btn_click_start_color)
+    btn_start.pack(fill='none', padx=8, side='top', in_=frame_4)
+    btn_end = tk.Button(root, text='end color', width=6, fg="#FF0000", background='#90caf9',
+                        command=btn_click_end_color)
+    btn_end.pack(fill='none', padx=8, side='bottom', in_=frame_4)
 
-    frame_left1.pack(side=tk.LEFT, expand=True)
-    frame_left2.pack(side=tk.LEFT, expand=True)
-    frame_right.pack(side=tk.LEFT, expand=True)
+    btn = tk.Button(root, text='>', width=6, command=btn_click_next)
+    btn.pack(fill='none', padx=8, side='right', in_=frame_5)
+    btn = tk.Button(root, text='<', width=6, command=btn_click_back)
+    btn.pack(fill='none', padx=8, side='right', in_=frame_5)
+    btn = tk.Button(root, text='▷', width=6, command=btn_click_auto)
+    btn.pack(fill='none', padx=8, side='right', in_=frame_5)
+    btn = tk.Button(root, text='↩︎', width=6, command=btn_click_restart)
+    btn.pack(fill='none', padx=8, side='right', in_=frame_5)
+
+    frame_1.pack(side=tk.LEFT, expand=True)
+    frame_2.pack(side=tk.LEFT, expand=True)
+    frame_3.pack(side=tk.LEFT, expand=True)
+    frame_4.pack(side=tk.LEFT, expand=True)
+    frame_5.pack(side=tk.LEFT, expand=True)
     root.after(10, auto_play)
     root.mainloop()
